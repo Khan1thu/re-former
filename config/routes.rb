9cb10cc98059a7cf6a599b43829c6e1f578a9003
Khan1thu <bienew311@gmail.com>
2022-09-20 09:47:13 +0630

    created controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "users#index"
-  resource :'users', only: [:new, :create]
+  resource :users, only: [:new, :create]
 end
