27908a215d43d7e1d02d7c80b791a6a3af8da22a
Khan1thu <bienew311@gmail.com>
2022-09-20 14:37:54 +0630

    finished form and created API work

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord  
-    validates :username, presence: true, length: { minimum: 2 }
+    validates :username, presence: true
+    validates :password, presence: true
     validates :email, presence: true
-    validates :password, presence: true, length: { maximum: 8 }  
 end
